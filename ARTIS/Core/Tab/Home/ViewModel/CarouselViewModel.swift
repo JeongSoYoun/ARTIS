8dfccbf701283e34f2162fe2de24cde4aad3fe8f
JeongSoYoun <socozy7210@gmail.com>
2021-09-29 02:43:06 +0900

    change ViewModel into single list and change func 'AnyViewReturn' to return anyview based on menu not on switch statement

--- ARTIS/Core/Tab/Home/ViewModel/CarouselViewModel.swift
+++ ARTIS/Core/Tab/Home/ViewModel/CarouselViewModel.swift
@@ -4,24 +4,16 @@ import SwiftUI
 
 class CarouselViewModel: ObservableObject {
     
-    @Published var sneakersVM: [NewsModel] = [
+    @Published var CarouselViewVM: [NewsModel] = [
         
         NewsModel(image_name: "peace", title: "피스마이너스원 에어포스 1", subject: "발매 정보"),
         NewsModel(image_name: "offwhite", title: "오프화이트 조던 1", subject: "발매 정보"),
         NewsModel(image_name: "sacai", title: "사카이 나이키", subject: "발매 정보"),
-        NewsModel(image_name: "travis", title: "트레비스 스캇 프라그먼트", subject: "발매 정보")
-    ]
-    
-    @Published var brandVM: [NewsModel] = [
-        
+        NewsModel(image_name: "travis", title: "트레비스 스캇 프라그먼트", subject: "발매 정보"),
         NewsModel(image_name: "sup", title: "슈프림 21 FW 룩북", subject: "브랜드"),
         NewsModel(image_name: "raf", title: "라프시몬스 프라다", subject: "브랜드"),
         NewsModel(image_name: "palace", title: "팔라스 스케이트보드 21FW 룩북", subject: "브랜드"),
-        NewsModel(image_name: "donda", title: "돈다 카니예 웨스트 투어 머크", subject: "브랜드")
-    ]
-    
-    @Published var exhibitionVM: [NewsModel] = [
-        
+        NewsModel(image_name: "donda", title: "돈다 카니예 웨스트 투어 머크", subject: "브랜드"),
         NewsModel(image_name: "yoshigo", title: "요시고 전시회", subject: "전시회"),
         NewsModel(image_name: "picasso", title: "피카소 전시회", subject: "전시회"),
         NewsModel(image_name: "lab", title: "랩 전시회", subject: "전시회"),
@@ -32,33 +24,21 @@ class CarouselViewModel: ObservableObject {
     
     func viewList(menu: String) -> ([AnyView],[String]) {
         
-        switch menu {
-            
-        case "발매 정보":
-            
-            return AnyViewReturn(vm: sneakersVM)
-            
-        case "브랜드":
-            
-            return AnyViewReturn(vm: brandVM)
-            
-        default:
-            
-            return AnyViewReturn(vm: exhibitionVM)
-        }
+        return AnyViewReturn(vm: CarouselViewVM, menu: menu)
     }
     
-    private func AnyViewReturn(vm: [NewsModel]) -> ([AnyView],[String]) {
+    private func AnyViewReturn(vm: [NewsModel], menu: String) -> ([AnyView],[String]) {
         
         var anyviewList: [AnyView] = []
         var titleList: [String] = []
         
         for index in (0 ..< vm.count) {
             
-            anyviewList.append(AnyView(Image(vm[index].image_name).resizable())
-            )
-            
-            titleList.append(vm[index].title)
+            if vm[index].subject == menu {
+                
+                anyviewList.append(AnyView(Image(vm[index].image_name).resizable()))
+                titleList.append(vm[index].title)
+            }
         }
         
         return (anyviewList,titleList)
